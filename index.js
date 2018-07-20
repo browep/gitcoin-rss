62a1efb9f09598e2672d5d1d4a5d69a097fe646a
Ubuntu <ubuntu@ip-172-31-45-157.us-west-2.compute.internal>
2018-07-20 17:49:39 +0000

    pulling contract from node

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const ethBaseUrl = `http://localhost:8545`;
+const contractAddress = `0x2af47a65da8CD66729b4209C22017d6A5C2d2400`;
+const abiArray = require('./contract-abi.json');
 
 const rp = require('request-promise');
 const Web3 = require('web3');
@@ -11,6 +13,12 @@ const web3 = new Web3(new Web3.providers.HttpProvider(ethBaseUrl));
 const nodeVersion = web3.version.node;
 
 console.log(`node version: ${nodeVersion}`);
+
+const BountyContract = web3.eth.contract(abiArray);
+
+const contractInstance = BountyContract.at(contractAddress);
+
+console.log(`contractInstance: ${contractInstance}`);
 
 
 
