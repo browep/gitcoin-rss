8095c3b25117de1860622dd88a7221de6af65d79
Ubuntu <ubuntu@ip-172-31-45-157.us-west-2.compute.internal>
2018-07-20 21:14:22 +0000

    writing out to file correctly

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const abiArray = require('./contract-abi.json');
 const rp = require('request-promise');
 const Web3 = require('web3');
 
+const outputFile = `/tmp/gitcoinrss.json`;
+
+
 const web3 = new Web3(new Web3.providers.HttpProvider(nconf.get('eth_address')));
 
 const nodeVersion = web3.version.node;
@@ -35,6 +38,7 @@ const numBounties = contractInstance.getNumBounties();
 console.log(`numBounties: ${numBounties}`);
 
 const bountyArray = [];
+var bountyWaitCount = numBounties;
 
 const createBountyDataCallback = i => {
     return (error, bountyDataId) => {
@@ -46,20 +50,44 @@ const createBountyDataCallback = i => {
         rp(`${nconf.get('ipfs_url_base')}${bountyDataId}`)
             .then((resBody) => {
                 const bountyData = JSON.parse(resBody);
-                if (bountyData && bountyData.payload) {
-                    console.log(`${i}=${bountyData.payload.title}`);}
+                if (bountyData && bountyData.title) {
+                    console.log(`${i}=${bountyData.title}`);
+                    bountyArray[i] = bountyData;
+                }
+                else if (bountyData && bountyData.payload) {
+                    console.log(`${i}=${bountyData.payload.title}`);
+                    bountyArray[i] = bountyData.payload;
+                }
                 else {
                     console.log(`${i}=undefined`);
                 }
 
+
+                onBountyRetrieval();
             })
             .catch(err => {
-                console.log(err);
+                console.log(`error: ${err}`);
+                onBountyRetrieval();
             })
     };
 } 
 
 for ( let i = 0; i < numBounties; i++) {
     contractInstance.getBountyData(i, createBountyDataCallback(i));
+}
+
+const onBountyRetrieval = () => {
+    bountyWaitCount--;
+    console.log(`bountWaitCount=${bountyWaitCount}`);
+
+    if (bountyWaitCount == 0) {
+        fs.writeFile(outputFile, JSON.stringify(bountyArray), (err) => {
+            if (err) throw err;
+
+            console.log(`wrote to ${outputFile}`);
+
+        });
+
+    }
 }
 
