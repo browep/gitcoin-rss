1b6ce8cad8c3c327d8200277b0f8ba1e70f3fc82
Ubuntu <ubuntu@ip-172-31-45-157.us-west-2.compute.internal>
2018-07-20 16:39:32 +0000

    can hit eth url and get version

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 'use strict';
 
+const rp = require('request-promise');
 
-console.log('started');
+const ethBaseUrl = `http://localhost:8545`;
+
+console.log(`started, eth url: ${ethBaseUrl}`);
+
+const versionOptions =  {
+    method: 'POST',
+    url: ethBaseUrl,
+    body: {
+        'jsonrpc': '2.0',
+        'method': 'web3_clientVersion',
+        'params': [],
+        'id': 67
+    },
+    json: true
+}
+
+rp(versionOptions)
+    .then( (jsonBody)=> {
+        console.log(jsonBody);
+
+    })
+    .catch( (err) => {
+        console.log(err);
+
+    });
+
 
 
