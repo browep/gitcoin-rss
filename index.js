4237f9b3464119c5b9a48750dd7db3d5c34c8535
Paul Brower <brower.paul@gmail.com>
2018-07-26 17:02:19 -0600

    rss feed uploading again

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const nconf = require('nconf');
 const Feed = require('feed');
 var _ = require('lodash');
 const format = require('./lib/format.js')
-const dao = require('./lib/dao.js');
 
 
 nconf.argv()
@@ -16,21 +15,37 @@ nconf.argv()
 const repository = require('./lib/repository.js')(nconf);
 const contract = require('./lib/contract.js')(nconf);
 
-const bountyDataPromises = [];
-for (let i = 0; i < 15 && i < contract.getNumBounties-1; i++) {
-    bountyDataPromises.push(repository.dataForBounty(contract.getNumBounties-1-i, contract));
+const bountyArray = [];
+for (let i = 0; i < 15 && i < contract.getNumBounties - 1; i++) {
+    bountyArray.push(repository.dataForBounty(contract.getNumBounties - 1 - i, contract));
 }
 
-Promise.all(bountyDataPromises)
-    .then((vals)=> {
-        console.log(`all bounty return values\n${vals}`);
+Promise.all(bountyArray)
+    .then((vals) => {
+        console.log(`num bounties: ${vals.length}`);
+
+        // sort the bounty array according to created date
+        let mappedVals = vals.map((val) => JSON.parse(val))
+        let sortedVals = mappedVals.sort((b, a) => {
+            if (a.payload.created < b.payload.created) {
+                return -1;
+            } else if (a.payload.created > b.payload.created) {
+                return 1;
+            } else { return 0; }
+        });
+
+        sortedVals.forEach((val) => console.log(`${val.payload.created}: ${val.payload.title}`));
+
+        createFeedAndUpload(sortedVals);
+
+
     })
-    .catch((err)=>{
+    .catch((err) => {
         console.error(`error: ${err.stack}`);
 
     });
 
-const onNewItems = () => {
+const createFeedAndUpload = (bountyArray) => {
     console.log(`creating new feed with ${bountyArray.length} bounties`);
 
     let feed = new Feed({
@@ -51,47 +66,22 @@ const onNewItems = () => {
     // if bountyArray does not have 15 elems, throw
     if (!bountyArray || bountyArray.length < 15) throw "expecting at least 15 bounties";
 
-    for (let i = 1; i < 16; i++) {
-        const bountyData = bountyArray[bountyArray.length - i];
+    bountyArray.forEach((bountyData) => {
         try {
             feed.addItem({
-                title: bountyData.title,
-                link: bountyData.webReferenceURL,
-                description: format.toHtml(_.get(bountyData, "metadata.issueKeywords", bountyData.description)),
-                content: format.toHtml(bountyData.description),
+                title: bountyData.payload.title,
+                link: bountyData.payload.webReferenceURL,
+                description: format.toHtml(_.get(bountyData, "payload.metadata.issueKeywords", bountyData.payload.description)),
+                content: format.toHtml(bountyData.payload.description),
             })
         } catch (err) {
-            console.error("issue with " + bountyData);
+            console.error("issue with " + bountyData + " " + err.stack);
         }
-    }
+    });
 
     const rssContent = feed.rss2();
 
-    const rssOutputPath = '/tmp/gitcoin.rss';
-    fs.writeFile(rssOutputPath, rssContent, (err) => {
-        if (err) throw err;
-        console.log(`wrote to ${rssOutputPath}`);
-
-        var params = {
-            localFile: rssOutputPath,
-
-            s3Params: {
-                Bucket: nconf.get('s3_bucket'),
-                Key: "feed.rss",
-            },
-        };
-        var uploader = s3Client.uploadFile(params);
-        uploader.on('error', function (err) {
-            console.error("unable to upload:", err.stack);
-        });
-        uploader.on('progress', function () {
-            console.log("rss upload progress", uploader.progressMd5Amount,
-                uploader.progressAmount, uploader.progressTotal);
-        });
-        uploader.on('end', function () {
-            console.log("done uploading rss file");
-        });
-    });
+    repository.uploadRss(rssContent);
 
 
 };
