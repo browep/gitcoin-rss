e6d6c31deb4b03230b5406780f0d8437aba38d76
Paul Brower <brower.paul@gmail.com>
2018-07-26 19:27:25 -0600

    moving to promises only

--- lambda.js
+++ lambda.js
@@ -6,8 +6,30 @@ exports.handler = async (event, context) => {
     console.log('value2 =', event.key2);
     console.log('value3 =', event.key3);
 
-    require('./gitcoin-rss.js');
-    
+
+    const options = {
+        method: 'POST',
+        url: "https://mainnet.infura.io/ROrDhANkiwtoZBhdXEtg",
+        body: {
+            jsonrpc: "2.0",
+            method: "web3_clientVersion",
+            params: [],
+            id: 67
+        },
+        json: true,
+    }
+
+    try {
+        const body = await require("request-promise").post(options);
+        console.log(`body: ${JSON.stringify(body)}`);
+        require('./gitcoin-rss.js');
+
+    } catch (e) {
+        console.log(`error: ${e.stack}`);
+    }
+
+
+
 
     return event.key1;  // Echo back the first key value
     // throw new Error('Something went wrong');
